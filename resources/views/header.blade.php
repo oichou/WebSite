6d500af6fcf5abe3b1c57023eb7713d57f1df8f0
safalakkab <safaalakkab7@gmail.com>
2020-04-20 01:49:04 +0000

    small changes in admin page

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -58,6 +58,11 @@
                         <li><a class="dropdown-item" href="{{ route('overview') }}">
                             Settings
                         </a></li>
+                        @if(Auth::user()->is_admin = true)
+                        <li><a class="dropdown-item" href="{{ route('admin.index') }}">
+                            Admin Panel
+                        </a></li>
+                        @endif
                         <li><a class="dropdown-item" href="{{ route('logout') }}"
                            onclick="event.preventDefault();
                                          document.getElementById('logout-form').submit();">
